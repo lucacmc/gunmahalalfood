e3741fbe9fbb68def7e731d43ce9da35c7566177
Kwaku <kwaku.nydev@gmail.com>
2022-06-24 02:18:03 +0700

    update layout

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -64,7 +64,7 @@
             </h2>
         <ul class="list-unstyled  mb-0 row gutters-5">
             @foreach ($featured_categories as $key => $category)
-                <li class="minw-0 col-4 col-md mt-3 cate_cell">
+                <li class="minw-0 col-4 col-md mt-3 cate_cell rounded hov-shadow-md">
                     <a href="{{ route('products.category', $category->slug) }}" class="d-block rounded bg-white p-2 text-reset shadow-sm">
                         <img
                             src="{{ static_asset('assets/img/placeholder.jpg') }}"
