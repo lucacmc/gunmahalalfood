1c0a63c8af4cdc1875d45ae49e2190f068357f19
Kwaku <kwaku.nydev@gmail.com>
2022-07-12 17:00:12 +0700

    add member point

--- app/Models/ClubPoint.php
+++ app/Models/ClubPoint.php
@@ -19,7 +19,7 @@ class ClubPoint extends Model
 
     public static function getUserPoint()
     {
-        if (Auth::user()->id) {
+        if (Auth::user() && Auth::user()->id) {
             $user_point = ClubPoint::where('user_id', Auth::user()->id)->where('convert_status', 0)->sum('points');
         } else {
             $user_point = 0;
